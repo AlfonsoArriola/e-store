3c355bc4fa6f051b87271028d669ba0fb176ccd6
Alfonso Arriola <alfonsoarriolajr@gmail.com>
2019-01-31 05:11:19 -0500

    work on product list commponent done.

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+import {Link} from 'react-router-dom';
+import {ProductConsumer} from '../context';
 
-export class Product extends Component {
+
+
+export default class Product extends Component {
 	render() {
+
+		const{id,title, img, price, inCart} = this.props.product;
+
 		return (
-			<div> <h3> I am a single product </h3></div>
+			<ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3"> 
+			  <h3> I am a single product </h3>
+			    <div className="card">
+			     <div className="img_container p-5" onClick={console.log('you clicked the card')}>
+			       <Link to='/details'>
+			        <img src={img} alt="product" className='card-img-top' />
+			       </Link>
+			     </div>
+			    </div>
+			</ProductWrapper>
 		);
 	}
 }
 
-export default Product;
+const ProductWrapper = styled.div`
+
+`
+
+
