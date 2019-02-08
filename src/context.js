fa69505c808c3299983e6964f07b06668ea6139f
Alfonso Arriola <alfonsoarriolajr@gmail.com>
2019-02-08 14:34:36 -0500

    context.js update

--- src/context.js
+++ src/context.js
@@ -1,26 +1,33 @@
 import React, { Component } from 'react';
 import { storeProducts, detailProduct } from './data';
-
 const ProductContext = React.createContext();
 // Provider
 // Consumer
 
-
  class ProductProvider extends Component {
-
- 	state={
- 		products:storeProducts, 
- 		detailProduct:detailProduct
+ 	state = {
+ 		products: [], 
+ 		detailProduct: detailProduct
+ 	};
+ 	componentDidMount() {
+ 		this.setProducts();
  	}
-
+ 	setProducts = () => {
+ 		let tempProducts = [];
+ 		storeProducts.forEach(item => {
+ 			const singleItem = {...item};
+ 			tempProducts = [...tempProducts, singleItem];
+ 		});
+ 		this.setState(() => {
+ 			return {products: tempProducts};
+ 		});
+ 	};
  	handleDetail = () =>{
  		console.log('hello from detail');
- 	}
-
+ 	};
  	addToCart = () =>{
  		console.log('hello from to cart');
- 	}
-
+ 	};
 
 	render() {
 		return (
