ee8293ddcb18a4a515e7d9ffec9d3775087f2fde
Alfonso Arriola <alfonsoarriolajr@gmail.com>
2019-02-24 12:37:16 -0500

    updated

--- src/context.js
+++ src/context.js
@@ -25,8 +25,8 @@ const ProductContext = React.createContext();
  	handleDetail = () =>{
  		console.log('hello from detail');
  	};
- 	addToCart = () =>{
- 		console.log('hello from to cart');
+ 	addToCart = (id) =>{
+ 		console.log(`hello from to cart.id is ${id}`);
  	};
 
 	render() {
